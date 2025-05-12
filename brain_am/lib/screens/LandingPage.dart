c81855dafcd92be100cd6cf868b97d9f7cd4d885
Anant Singhal <anantsinghal444@gmail.com>
2025-05-12 17:59:18 +0530

    hero containers

--- brain_am/lib/screens/LandingPage.dart
+++ brain_am/lib/screens/LandingPage.dart
@@ -1,6 +1,7 @@
 import 'package:brain.am/widgets/DemoImage.dart';
 import 'package:brain.am/widgets/HeaderText.dart';
 import 'package:brain.am/widgets/HeroText.dart';
+import 'package:brain.am/widgets/MainContainer.dart';
 import 'package:brain.am/widgets/StartListeningButton.dart';
 import 'package:flutter/material.dart';
 import 'package:glassmorphism/glassmorphism.dart';
@@ -44,71 +45,3 @@ class BrainAMHome extends StatelessWidget {
 }
 
 
-
-class MainContainer extends StatelessWidget {
-  const MainContainer({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GlassmorphicContainer(
-      width: MediaQuery.of(context).size.width * 0.8,
-      height: MediaQuery.of(context).size.height * 0.9,
-      borderRadius: 25,
-      linearGradient: LinearGradient(
-        begin: Alignment.centerLeft,
-        end: Alignment.topRight,
-        colors: [
-          const Color.fromARGB(255, 132, 132, 132).withAlpha(20),
-          const Color.fromARGB(255, 94, 94, 94).withAlpha(13),
-        ],
-      ),
-      border: 0.6,
-      blur: 0,
-      borderGradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          const Color.fromARGB(255, 175, 175, 175).withAlpha(77),
-          const Color.fromARGB(0, 0, 0, 0).withAlpha(26),
-        ],
-      ),
-    
-      child: MainContainerContent(),
-    );
-  }
-}
-
-class MainContainerContent extends StatelessWidget {
-  const MainContainerContent({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        HeaderText(),
-        
-        SizedBox(height: 10),
-        
-        Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.43,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                HeroText(text: "Sounds For \nEverything You Do"),
-                SizedBox(height: 25),
-                StartListeningButton(),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-}
--- brain_am/lib/widgets/MainContainer.dart
+++ brain_am/lib/widgets/MainContainer.dart
@@ -1,4 +1,5 @@
 import 'package:brain.am/screens/LandingPage.dart';
+import 'package:brain.am/widgets/MainContainerContent.dart';
 import 'package:flutter/material.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 
