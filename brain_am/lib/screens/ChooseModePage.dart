4e02d8cd921ac4769c5367d08b60ae28d70c6cc8
Anant Singhal <anantsinghal444@gmail.com>
2025-05-13 10:10:21 +0530

    a

--- brain_am/lib/screens/ChooseModePage.dart
+++ brain_am/lib/screens/ChooseModePage.dart
@@ -25,35 +25,16 @@ class ChooseModePage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  GlassmorphicContainer(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    height: MediaQuery.of(context).size.height * 0.5,
-                    borderRadius: 25,
-                    linearGradient: LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.topRight,
-                      colors: [
-                        const Color.fromARGB(255, 132, 132, 132).withAlpha(20),
-                        const Color.fromARGB(255, 94, 94, 94).withAlpha(13),
-                      ],
-                    ),
-                    border: 0.6,
-                    blur: 0,
-                    borderGradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        const Color.fromARGB(255, 175, 175, 175).withAlpha(77),
-                        const Color.fromARGB(0, 0, 0, 0).withAlpha(26),
-                      ],
-                    ),
-          
-                    child: Text('focus'),
-                  ),
-          
+                  BigGlassMorphContainer(text: 'Focus',),
+
                   Column(
-                    children: [Text('Sleep'), Text('Study'), Text('Relax')],
+                    children: [
+                      SmallGlassMorphContainer(text: 'Chill'),
+                      SmallGlassMorphContainer(text: 'Sleep'),
+                      SmallGlassMorphContainer(text: 'Study'),
+                    ],
                   ),
                 ],
               ),
@@ -61,6 +42,87 @@ class ChooseModePage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class BigGlassMorphContainer extends StatelessWidget {
+  const BigGlassMorphContainer({
+    super.key, required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassmorphicContainer(
+      margin: EdgeInsets.only(right: 20, bottom: 20),
+      width: MediaQuery.of(context).size.width * 0.3,
+      height: MediaQuery.of(context).size.height * 0.56,
+      borderRadius: 25,
+      linearGradient: LinearGradient(
+        begin: Alignment.centerLeft,
+        end: Alignment.topRight,
+        colors: [
+          const Color.fromARGB(255, 132, 132, 132).withAlpha(20),
+          const Color.fromARGB(255, 94, 94, 94).withAlpha(13),
+        ],
+      ),
+      border: 0.6,
+      blur: 0,
+      borderGradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          const Color.fromARGB(255, 175, 175, 175).withAlpha(77),
+          const Color.fromARGB(0, 0, 0, 0).withAlpha(26),
+        ],
+      ),
+    
+      child: Text(text, style: TextStyle(color: Colors.white)),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+class SmallGlassMorphContainer extends StatelessWidget {
+  const SmallGlassMorphContainer({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassmorphicContainer(
+      width: MediaQuery.of(context).size.width * 0.3,
+      height: MediaQuery.of(context).size.height * 0.17,
+      margin: EdgeInsets.only(bottom: 20),
+      borderRadius: 25,
+      linearGradient: LinearGradient(
+        begin: Alignment.centerLeft,
+        end: Alignment.topRight,
+        colors: [
+          const Color.fromARGB(255, 132, 132, 132).withAlpha(20),
+          const Color.fromARGB(255, 94, 94, 94).withAlpha(13),
+        ],
+      ),
+      border: 0.6,
+      blur: 0,
+      borderGradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          const Color.fromARGB(255, 175, 175, 175).withAlpha(77),
+          const Color.fromARGB(0, 0, 0, 0).withAlpha(26),
+        ],
+      ),
+
+      child: Text(text, style: TextStyle(color: Colors.white)),
     );
   }
 }
