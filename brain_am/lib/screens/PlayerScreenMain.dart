298e4e63f2dbcaf26869969b8bed6a51c972891b
Anant Singhal <anantsinghal444@gmail.com>
2025-05-13 18:49:44 +0530

    new file

---
+++ brain_am/lib/screens/PlayerScreenMain.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlayerScreenMain extends StatefulWidget {
+  const PlayerScreenMain({super.key});
+
+  @override
+  State<PlayerScreenMain> createState() => _PlayerScreenMainState();
+}
+
+class _PlayerScreenMainState extends State<PlayerScreenMain> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
